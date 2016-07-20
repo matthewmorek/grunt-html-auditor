3db8486649f925f0a1060666d7abf4b02713f351
Matthew Morek <me@matthewmorek.com>
2016-07-20 11:00:29 +0100

    Log the exact cmd being executed via `execFile`

--- tasks/lib/audit-a11y.js
+++ tasks/lib/audit-a11y.js
@@ -46,6 +46,7 @@ module.exports = function (data, done) {
   var bin = process.cwd() + '/node_modules/.bin/html-audit';
   execFile(bin, ['a11y', '--path', data.file.file], function (error, result, code) {
     if (error) {
+      data.grunt.log.writeln('Running:' + bin + ' a11y --path ' + data.file.file);
       data.grunt.log.writeln(chalk.red(result));
       data.grunt.log.writeln(chalk.red(code));
       data.grunt.fail.fatal(result, 1);
